4fcb093f1a2f76d62b10e326ce2b709396dc17bb
Rujia <rxl635@student.bham.ac.uk>
2018-01-28 03:04:26 +0000

    update api name

--- consensus/prometheus/api.go
+++ consensus/prometheus/api.go
@@ -1,18 +1,18 @@
-// Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// Copyright 2018 The go-hpb Authors
+// This file is part of the go-hpb.
 //
-// The go-ethereum library is free software: you can redistribute it and/or modify
+// The go-hpb is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The go-hpb is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// along with the go-hpb. If not, see <http://www.gnu.org/licenses/>.
 
 package prometheus
 
@@ -24,14 +24,12 @@ import (
 	"github.com/hpb-project/go-hpb/rpc"
 )
 
-// API is a user facing RPC API to allow controlling the signer and voting
-// mechanisms of the proof-of-authority scheme.
+
 type API struct {
 	chain  consensus.ChainReader
 	prometheus *Prometheus
 }
 
-// GetHistorysnap retrieves the state snapshot at a given block.
 func (api *API) GetHistorysnap(number *rpc.BlockNumber) (*Historysnap, error) {
 	// Retrieve the requested block number (or current if none requested)
 	var header *types.Header
@@ -47,7 +45,6 @@ func (api *API) GetHistorysnap(number *rpc.BlockNumber) (*Historysnap, error) {
 	return api.prometheus.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
 }
 
-// GetHistorysnapAtHash retrieves the state snapshot at a given block.
 func (api *API) GetHistorysnapAtHash(hash common.Hash) (*Historysnap, error) {
 	header := api.chain.GetHeaderByHash(hash)
 	if header == nil {
@@ -56,7 +53,6 @@ func (api *API) GetHistorysnapAtHash(hash common.Hash) (*Historysnap, error) {
 	return api.prometheus.snapshot(api.chain, header.Number.Uint64(), header.Hash(), nil)
 }
 
-// GetSigners retrieves the list of authorized signers at the specified block.
 func (api *API) GetSigners(number *rpc.BlockNumber) ([]common.Address, error) {
 	// Retrieve the requested block number (or current if none requested)
 	var header *types.Header
@@ -76,7 +72,6 @@ func (api *API) GetSigners(number *rpc.BlockNumber) ([]common.Address, error) {
 	return snap.signers(), nil
 }
 
-// GetSignersAtHash retrieves the state snapshot at a given block.
 func (api *API) GetSignersAtHash(hash common.Hash) ([]common.Address, error) {
 	header := api.chain.GetHeaderByHash(hash)
 	if header == nil {
@@ -89,7 +84,6 @@ func (api *API) GetSignersAtHash(hash common.Hash) ([]common.Address, error) {
 	return snap.signers(), nil
 }
 
-// Proposals returns the current proposals the node tries to uphold and vote on.
 func (api *API) Proposals() map[common.Address]bool {
 	api.prometheus.lock.RLock()
 	defer api.prometheus.lock.RUnlock()
@@ -101,8 +95,6 @@ func (api *API) Proposals() map[common.Address]bool {
 	return proposals
 }
 
-// Propose injects a new authorization proposal that the signer will attempt to
-// push through.
 func (api *API) Propose(address common.Address, auth bool) {
 	api.prometheus.lock.Lock()
 	defer api.prometheus.lock.Unlock()
@@ -115,8 +107,6 @@ func (api *API) Propose(address common.Address, auth bool) {
 	api.prometheus.proposals[address] = auth
 }
 
-// Discard drops a currently running proposal, stopping the signer from casting
-// further votes (either for or against).
 func (api *API) Discard(address common.Address) {
 	api.prometheus.lock.Lock()
 	defer api.prometheus.lock.Unlock()
